7a56de54914966ab553a20086f04c356c2aa4257
root <root@ip-172-31-81-61.ec2.internal>
2025-03-05 07:30:19 +0000

    add content

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const os = require("os");
-const axios = require("axios");
 
 const app = express();
 
 // 根路由
 app.get("/", (req, res) => {
-  res.send("<h1>Hey guys, Welcome to my new Express Application !! update1</h1>");
+  res.send(`
+    <h1>Hey guys, Welcome to my new Express Application !!</h1>
+    <p>For more information, visit <a href='/system-info'>/system-info</a></p>
+  `);
 });
 
 // /info 路由
@@ -14,42 +16,20 @@ app.get("/info", (req, res) => {
   res.send("<h1>This is a simple info page. For more information, visit <a href='/system-info'>/system-info</a></h1>");
 });
 
-// 新增的路由，返回本机的信息（容器的 IP 和 ECS 任务元数据）
-app.get("/system-info", async (req, res) => {
-  try {
-    // 获取 ECS 任务的元数据
-    const metadataResponse = await axios.get("http://169.254.170.2/v2/metadata");
+// 新增的路由，返回本机的信息（系统的基本信息）
+app.get("/system-info", (req, res) => {
+  const systemInfo = {
+    platform: os.platform(),  // 操作系统平台 (如 linux, darwin, win32)
+    arch: os.arch(),          // 系统架构 (如 x64)
+    cpu: os.cpus(),           // CPU 信息
+    memory: os.totalmem(),    // 总内存
+    freeMemory: os.freemem(), // 剩余内存
+    hostname: os.hostname(),  // 主机名
+    networkInterfaces: os.networkInterfaces(), // 网络接口信息
+    uptime: os.uptime()       // 系统启动时间
+  };
 
-    // 打印返回的数据，查看 ECS 元数据
-    console.log(metadataResponse.data);
-
-    // 检查 metadataResponse 是否包含期望的数据
-    if (!metadataResponse.data || !metadataResponse.data.Containers || metadataResponse.data.Containers.length === 0) {
-      return res.status(500).json({ error: "ECS metadata response is missing containers information" });
-    }
-
-    // 获取容器的 IP 地址
-    const containerIP = metadataResponse.data.Containers[0].networkInterfaces[0].ipv4Addresses[0];
-
-    // 收集系统信息
-    const systemInfo = {
-      platform: os.platform(),
-      arch: os.arch(),
-      cpu: os.cpus(),
-      memory: os.totalmem(),
-      freeMemory: os.freemem(),
-      hostname: os.hostname(),
-      containerIP: containerIP, // 添加容器的 IP 地址
-      networkInterfaces: os.networkInterfaces(),
-      uptime: os.uptime(),
-      ecsMetadata: metadataResponse.data, // 包含 ECS 任务的详细信息
-    };
-
-    res.json(systemInfo); // 以 JSON 格式返回系统信息
-  } catch (error) {
-    console.error("Error fetching ECS metadata:", error);
-    res.status(500).json({ error: "Error fetching ECS metadata", details: error.message });
-  }
+  res.json(systemInfo); // 以 JSON 格式返回系统信息
 });
 
 // 启动监听3000端口
