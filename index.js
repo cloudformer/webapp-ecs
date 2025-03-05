fd34233e15cec42eb5e206366a900bd7cd1fd150
root <root@ip-172-31-81-61.ec2.internal>
2025-03-05 01:21:49 +0000

    update index 1

--- index.js
+++ index.js
@@ -4,23 +4,29 @@ const axios = require("axios");
 
 const app = express();
 
-// 根路由
+// Root route
 app.get("/", (req, res) => {
-  res.send("<h1>Hey guys, Welcome to my new Express Application!!</h1>");
+  res.send("<h1>Hey guys, Welcome to my new Express Application !! update1</h1>");
 });
 
-// /info 路由
+// /info route
 app.get("/info", (req, res) => {
   res.send("hey this is /info api");
 });
 
-// 新增的路由，返回本机的信息（容器的 IP 和 ECS 任务元数据）
+// New route to return local system information (container IP and ECS task metadata)
 app.get("/system-info", async (req, res) => {
   try {
-    // 获取 ECS 任务的元数据
+    // Fetch ECS task metadata
     const metadataResponse = await axios.get("http://169.254.170.2/v2/metadata");
-    const containerIP = metadataResponse.dataContainers[0].networkInterfaces[0].ipv4Addresses[0]; // 获取容器的 IP 地址
     
+    let containerIP = null;
+    // If the metadata response contains networkInterfaces and ipv4Addresses, get the container's IP address
+    if (metadataResponse.data && metadataResponse.data.Containers && metadataResponse.data.Containers[0].networkInterfaces) {
+      const networkInterfaces = metadataResponse.data.Containers[0].networkInterfaces;
+      containerIP = networkInterfaces[0]?.ipv4Addresses?.[0] || "IP not found";
+    }
+
     const systemInfo = {
       platform: os.platform(),
       arch: os.arch(),
@@ -28,20 +34,20 @@ app.get("/system-info", async (req, res) => {
       memory: os.totalmem(),
       freeMemory: os.freemem(),
       hostname: os.hostname(),
-      containerIP: containerIP, // 添加容器的 IP 地址
+      containerIP: containerIP, // Add the container's IP address
       networkInterfaces: os.networkInterfaces(),
       uptime: os.uptime(),
-      ecsMetadata: metadataResponse.data, // 包含 ECS 任务的详细信息
+      ecsMetadata: metadataResponse.data, // Contains detailed ECS task information
     };
 
-    res.json(systemInfo); // 以 JSON 格式返回系统信息
+    res.json(systemInfo); // Return system information in JSON format
   } catch (error) {
     console.error("Error fetching ECS metadata:", error);
     res.status(500).send("Error fetching ECS metadata");
   }
 });
 
-// 启动监听3000端口
+// Start listening on port 3000
 app.listen(3000, () => {
   console.log("listening on port 3000");
 });
