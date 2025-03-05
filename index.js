a368cad45883acbfee32c4e9e57bb69e52eb3847
root <root@ip-172-31-81-61.ec2.internal>
2025-03-05 02:41:05 +0000

    add v1

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hey guys, Welcome to my new Express Application!!</h1>");
+  res.send("<h1>Hey guys,v1   Welcome to my new Express Application!!</h1>");
 });
 
 app.get("/info", (req, res) => {
