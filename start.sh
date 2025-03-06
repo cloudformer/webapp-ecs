d9796f28a2dcbed24aac0089b6bdb46274ebb4ad
root <root@ip-172-31-81-61.ec2.internal>
2025-03-06 05:28:19 +0000

    update start

--- start.sh
+++ start.sh
@@ -1,8 +1,11 @@
-#!/bin/bash
-
+#!/bin/sh
 # 定期检查 S3 上的 index.js 文件是否有更新
 aws s3 cp s3://codeforautomation/ecsnodejs/index.js /express-app/index.js
-pwd
-# 启动 Node.js 应用
-node /express-app/ndex.js
 
+#复制完index
+ls 
+ls /express-app
+
+echo "Starting the app..."
+
+node /express-app/index.js || echo "Error running index.js"
