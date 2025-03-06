b956bb06a24b9a629fd65d2ac1b2d2cc626b561c
root <root@ip-172-31-81-61.ec2.internal>
2025-03-06 05:50:11 +0000

    ecs task add s3 iam

--- start.sh
+++ start.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 # 定期检查 S3 上的 index.js 文件是否有更新
+echo "Fetching index.js from S3..."
 aws s3 cp s3://codeforautomation/ecsnodejs/index.js /express-app/index.js
 
-#复制完index
-ls 
+# 确保文件被正确下载
+echo "Listing /express-app directory:"
 ls /express-app
+
+# 查看 index.js 的内容
+echo "Content of index.js:"
+cat /express-app/index.js
 
 echo "Starting the app..."
 
-node /express-app/index.js || echo "Error running index.js"
+# 运行 index.js，并捕获错误输出
+node /express-app/index.js 2>&1 || echo "Error running index.js"
+
